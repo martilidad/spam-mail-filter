6cfacad3b7b9c89929029c3ee39992395c58a524
Stefan Schiechel <stefanschiechel@gmx.de>
2019-12-07 18:31:13 +0100

    added uidvalidity value to tracked uids

--- core/MailChecker.py
+++ core/MailChecker.py
@@ -1,6 +1,6 @@
 import logging
 import threading
-from typing import List
+from typing import List, Tuple, Dict
 
 from core.CheckMode import CheckMode
 from imap.ImapClient import ImapClient
@@ -19,27 +19,33 @@ class MailChecker(threading.Thread):
         while not self.stopped.wait(self.config.check_interval * 60):
             self.__mail_check()
 
-    def __retrieve_new_uids(self, imap):
+    def __retrieve_new_uids(self, imap, mbid: str) -> Tuple[List[bytes], Dict[str, List[int]]]:
         uids = imap.get_all_uids()
-        old_checked_uids = SerializationUtils.deserialize(
+        tracked_uids = SerializationUtils.deserialize(
             self.config.trackfile)
-        if old_checked_uids is None:
-            old_checked_uids = []
-        new_uids = [u for u in uids if int(u) not in old_checked_uids]
-        return new_uids, old_checked_uids
+        if tracked_uids is None:
+            tracked_uids = {mbid: []}
+        elif type(tracked_uids) is not dict:
+            tracked_uids = {mbid: []}
+        elif mbid not in tracked_uids:
+            tracked_uids[mbid] = []
+        new_uids = [u for u in uids if int(u) not in tracked_uids[mbid]]
+        return new_uids, tracked_uids
 
-    def __store_new_checke_uids(self, old_checked_uids: List[int],
+    def __store_new_checked_uids(self, mbid: str, tracked_uids: dict,
                                 new_uids: List[bytes]):
-        new_checked_uids = list(
-            set(old_checked_uids + [int(u) for u in new_uids]))
-        SerializationUtils.serialize(new_checked_uids, self.config.trackfile)
+        new_checked_uids_for_mailbox = list(
+            set(tracked_uids[mbid] + [int(u) for u in new_uids]))
+        tracked_uids[mbid] = new_checked_uids_for_mailbox
+        SerializationUtils.serialize(tracked_uids, self.config.trackfile)
 
     def __mail_check(self):
         logging.info("checking mails")
         imap = ImapClient(self.config.host, self.config.port)
         imap.login(self.config.username, self.config.password)
         imap.select_mailbox(self.config.inbox)
-        new_uids, old_checked_uids = self.__retrieve_new_uids(imap)
+        mbid = str(imap.get_mailbox_identifier(self.config.inbox))
+        new_uids, tracked_uids = self.__retrieve_new_uids(imap, mbid)
         for i in range(0, len(new_uids), self.config.batch_size):
             uids = new_uids[i:i + self.config.batch_size]
             messages = imap.get_mails_for_uids(uids)
@@ -55,7 +61,7 @@ class MailChecker(threading.Thread):
                     # else DRYRUN: nothing to do
                 else:
                     logging.debug("ham detected")
-        self.__store_new_checke_uids(old_checked_uids, new_uids)
+        self.__store_new_checked_uids(mbid, tracked_uids, new_uids)
         imap.logout()
         logging.debug("mailcheck complete")
 
